233178a7c05c377ac77cdd234ff5bf95bb4302b5
Rishabh Pal <rish.09.rp@gmail.com>
2023-11-03 14:28:30 +0530

    updated

--- Personal Practice/EquationSolver.m
+++ Personal Practice/EquationSolver.m
@@ -4,4 +4,11 @@ x = linspace(0,5);
 y = ((-(x-3).^2)) + 10;
 
 plot(x,y,'.')
-max(y)
+[MaxVal, I] = max(y)
+
+x_value = x(I)
+
+%to get the value of y at x = 20.7
+y = @(x) ((-(x-3).^2)) + 10 ;
+
+y(20.7)
